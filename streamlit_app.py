0c7c944385e551f8af2ae4ff54e7c046ed1540d6
andrewho0423 <48813730+andrewho0423@users.noreply.github.com>
2025-01-11 10:01:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.set_page_config(
 st.title("魔法使的約定--活動覺醒小人特效%數計算機")
 
 # Initialize some parameters
-type_0, type_1, type_2, type_3, type_4 = "抽卡", "抽卡", "抽卡", "抽卡", "抽卡"
-totsu_0, totsu_1, totsu_2, totsu_3, totsu_4 = 0,0,0,0,0
+type_0, type_1, type_2, type_3, type_4, type_5 = "抽卡", "抽卡", "抽卡", "抽卡", "抽卡", "抽卡"
+totsu_0, totsu_1, totsu_2, totsu_3, totsu_4, totsu_5 = 0,0,0,0,0,0
 
 event = st.radio(
     label = "請選擇活動類型",
@@ -108,14 +108,14 @@ if event == "祭典(祝祭)":
             key="totsu_3"
         )
     team_4 = st.radio(
-        label="請選擇好友的稀有度與突破數",
-        options=["非活動卡", "SSR(自己)", "SR(自己)", "R(自己)", "好友卡(默認SSR)"],
+        label="請選擇隊員卡3的稀有度與突破數",
+        options=["非活動卡", "SSR", "SR", "R"],
         horizontal=True,
         key="team_4"
     )
-    if team_4 not in ["非活動卡", "好友卡(默認SSR)"]:
+    if team_4 != "非活動卡":
         type_4 = st.radio(
-            label="請選擇好友卡的獲得方式",
+            label="請選擇隊員卡4的獲得方式",
             options=["抽卡", "報酬"],
             horizontal=True,
             key="type_4"
@@ -126,6 +126,26 @@ if event == "祭典(祝祭)":
             max_value=5,
             value="min",
             key="totsu_4"
+        )
+    team_5 = st.radio(
+        label="請選擇好友的稀有度與突破數",
+        options=["非活動卡", "SSR(自己)", "SR(自己)", "R(自己)", "好友卡(默認SSR)"],
+        horizontal=True,
+        key="team_5"
+    )
+    if team_5 not in ["非活動卡", "好友卡(默認SSR)"]:
+        type_5 = st.radio(
+            label="請選擇好友卡的獲得方式",
+            options=["抽卡", "報酬"],
+            horizontal=True,
+            key="type_5"
+        )
+        totsu_5 = st.number_input(
+            label="請選擇突破數",
+            min_value=0,
+            max_value=5,
+            value="min",
+            key="totsu_5"
         )
 elif event == "奏鳴曲(ソナチネ)":
     kakusei_rarity = st.radio(
@@ -150,15 +170,18 @@ if event == "祭典(祝祭)":
             team_2=team_2,
             team_3=team_3,
             team_4=team_4,
+            team_5=team_5,
             type_0=type_0,
             type_1=type_1,
             type_2=type_2,
             type_3=type_3,
             type_4=type_4,
+            type_5=type_5,
             totsu_0=totsu_0,
             totsu_1=totsu_1,
             totsu_2=totsu_2,
             totsu_3=totsu_3,
             totsu_4=totsu_4,
+            totsu_5=totsu_5,
         )
         st.write("您的活動小人特效%數是: "+str(percentage)+"%")
