5efc2a2fcbf65060651f26622658c2d5c16d9540
andrewho0423 <48813730+andrewho0423@users.noreply.github.com>
2025-01-11 08:44:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ st.title("魔法使的約定--活動覺醒小人特效%數計算機")
 
 event = st.radio(
     label = "請選擇活動類型",
-    options = ["一般活動", "祭典(祝祭)", "奏鳴曲(ソナチネ)"]
+    options = ["一般活動", "祭典(祝祭)", "奏鳴曲(ソナチネ)"],
+    horizontal=True
 )
